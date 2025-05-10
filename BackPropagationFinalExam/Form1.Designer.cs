507c0437951e6e4438671e46fb392513b0266c3b
Violdan Bayocot <decr0zeath56@gmail.com>
2025-05-10 09:52:49 +0800

    - Added form controls

--- BackPropagationFinalExam/Form1.Designer.cs
+++ BackPropagationFinalExam/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbInput1 = new System.Windows.Forms.ComboBox();
+            this.cmbInput2 = new System.Windows.Forms.ComboBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(49, 124);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(121, 34);
+            this.btnTrain.TabIndex = 0;
+            this.btnTrain.Text = "Train";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(186, 124);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(121, 34);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input 2";
+            // 
+            // cmbInput1
+            // 
+            this.cmbInput1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbInput1.FormattingEnabled = true;
+            this.cmbInput1.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.cmbInput1.Location = new System.Drawing.Point(103, 34);
+            this.cmbInput1.Name = "cmbInput1";
+            this.cmbInput1.Size = new System.Drawing.Size(67, 21);
+            this.cmbInput1.TabIndex = 4;
+            // 
+            // cmbInput2
+            // 
+            this.cmbInput2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbInput2.FormattingEnabled = true;
+            this.cmbInput2.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.cmbInput2.Location = new System.Drawing.Point(103, 77);
+            this.cmbInput2.Name = "cmbInput2";
+            this.cmbInput2.Size = new System.Drawing.Size(67, 21);
+            this.cmbInput2.TabIndex = 5;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(225, 9);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(48, 13);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "Output : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(362, 187);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.cmbInput2);
+            this.Controls.Add(this.cmbInput1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnTrain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbInput1;
+        private System.Windows.Forms.ComboBox cmbInput2;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
